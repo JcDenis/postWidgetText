c45d7f40ec000d00bca761b440e8ac0e3023dda3
Jean-Christian Denis <contact@jcdenis.fr>
2023-04-25 15:05:47 +0200

    cosmetic

--- js/backend.js
+++ js/backend.js
@@ -2,17 +2,19 @@
 'use strict';
 
 $(() => {
-	$('#post-wtext-form h4').toggleWithLegend(
-	  $('#post-wtext-form').children().not('h4'),
-	  {
-	    user_pref:'dcx_pwt_form',
-	    legend_click:true
-	  }
-	);
-	if ($.isFunction(jsToolBar)) {
-	  $('#post_wtext').each(function() {
-	    var tbWidgetText = new jsToolBar(this);
-	    tbWidgetText.draw('xhtml');
-	  });
-	}
+  $('#post-wtext-form h4').toggleWithLegend(
+    $('#post-wtext-form').children().not('h4'),
+    {
+      user_pref:'dcx_pwt_form',
+      legend_click:true
+    }
+  );
+
+  $('#post_wtext').each(function () {
+    if (typeof jsToolBar === 'function') {
+      const tbWidgetText = new jsToolBar(this);
+      tbWidgetText.context = 'pwt';
+      tbWidgetText.draw('xhtml');
+    }
+  });
 });
